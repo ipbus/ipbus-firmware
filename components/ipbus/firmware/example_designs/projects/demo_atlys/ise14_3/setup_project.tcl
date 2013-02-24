c891b8db323ef8e2c31f8cc46445bd01436ab674
Dave Newbold <Dave.Newbold@cern.ch>
2013-02-24 18:23:23 +0000

    Adding new sim-friendly build script     [CACTUS SVN 8566]

--- components/ipbus/firmware/example_designs/projects/demo_atlys/ise14_3/setup_project.tcl
+++ components/ipbus/firmware/example_designs/projects/demo_atlys/ise14_3/setup_project.tcl
@@ -1,3 +1,20 @@
+# Horrible hacky TCL script to build ISE project from hierarchy of source lists
+
+proc addsources {file base} {
+	set fp [open $file r]
+	set files [read $fp]
+	close $fp
+	set flist [split $files "\n"]
+	foreach f $flist {
+		if {$f == "" || string index $f 0 == #} continue
+		if ($base != "") {
+			xfile add $base/[exec basename $f]
+		} else {
+			xfile add $f
+		}
+	}
+}
+
 project new demo_atlys
 project set family spartan6
 project set device xc6slx45
@@ -9,24 +26,9 @@ project set "Pack I/O Registers/Latches into IOBs" "For Inputs and Outputs" -pro
 project set "Enable Multi-Threading" "2" -process "Place & Route"
 project set "Enable BitStream Compression" TRUE -process "Generate Programming File"
 
-xfile add firmware/ipbus/firmwareexample_designs/hdl/demo_atlys/top_atlys.vhd
-xfile add firmware/ipbus/firmwareexample_designs/ucf/atlys.ucf
-xfile add firmware/ipbus/firmwareexample_designs/hdl/clocks_s6_extphy_100MHz.vhd
-xfile add firmware/ipbus/firmwareexample_designs/hdl/clock_divider_s6.v
-xfile add firmware/ipbus/firmwareethernet/hdl/emac_hostbus_decl.vhd
-xfile add firmware/ipbus/firmwareethernet/hdl/eth_s6_gmii.vhd
-
-source firmware/ipbus/firmwareipbus_core/cfg/setup.tcl
-source firmware/ipbus/firmwareexample_designs/cfg/setup.tcl
-
-set fp [open cores_list r]
-set files [read $fp]
-close $fp
-set flist [split $files "\n"]
-foreach f $flist {
-	if {$f eq ""} continue
-	set fb [exec basename $f]
-	xfile add ipcore_dir/$fb
-}
+addsources file_list ""
+addsources firmware/ipbus/firmware/ipbus_core/cfg/file_list ""
+addsources firmware/ipbus/firmware/example_designs/cfg/file_list ""
+addsources cores_list "ipcore_dir"
 
 project close
