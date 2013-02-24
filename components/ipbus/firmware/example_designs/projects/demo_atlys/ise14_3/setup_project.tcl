15d4d9e91ba05ce0d567f7af9521076338a124f5
Dave Newbold <Dave.Newbold@cern.ch>
2013-02-24 20:01:39 +0000

    Bug fix     [CACTUS SVN 8571]

--- components/ipbus/firmware/example_designs/projects/demo_atlys/ise14_3/setup_project.tcl
+++ components/ipbus/firmware/example_designs/projects/demo_atlys/ise14_3/setup_project.tcl
@@ -1,19 +1,27 @@
 # Horrible hacky TCL script to build ISE project from hierarchy of source lists
 
-proc addsources {file base} {
+proc foreachinfile {file cmd args} {
 	set fp [open $file r]
 	set files [read $fp]
 	close $fp
 	set flist [split $files "\n"]
 	foreach f $flist {
 		if {$f == "" || [string index $f 0] == "#"} continue
-		if {$base != ""} {
-			xfile add ${base}/[exec basename $f]
-		} else {
-			xfile add $f
-		}
+		$cmd $args
 	}
 }
+
+proc addfile {file} {
+	xfile add $file
+}
+
+proc addcore {file base} {
+	set bname [exec basename $file]
+	exec cp ../${file} ipcore_dir
+	cd ipcore_dir
+	exec coregen -r -b $bname -p coregen.cgp
+	cd ..
+	xfile add ${base}/$bname
 
 project new demo_atlys
 project set family spartan6
@@ -26,9 +34,9 @@ project set "Pack I/O Registers/Latches into IOBs" "For Inputs and Outputs" -pro
 project set "Enable Multi-Threading" "2" -process "Place & Route"
 project set "Enable BitStream Compression" TRUE -process "Generate Programming File"
 
-addsources file_list ""
-addsources firmware/ipbus/firmware/ipbus_core/cfg/file_list ""
-addsources firmware/ipbus/firmware/example_designs/cfg/file_list ""
-addsources cores_list "ipcore_dir"
+foreachinfile file_list addfile 
+foreachinfile firmware/ipbus/firmware/ipbus_core/cfg/file_list addfile
+foreachinfile firmware/ipbus/firmware/example_designs/cfg/file_list addfile
+foreachinfile cores_list addcore ipcore_dir
 
 project close
