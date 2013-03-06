77d368d3b7c8c2e76a51761cc3d691d852230fdc
Dave Newbold <Dave.Newbold@cern.ch>
2013-03-06 15:12:20 +0000

    Adding build scripts     [CACTUS SVN 20082]

--- components/ipbus/firmware/example_designs/scripts/build_all_designs.sh
+++ components/ipbus/firmware/example_designs/scripts/build_all_designs.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 for dir in `ls ipbus/firmware/example_designs/projects`; do
-	echo $dir
+	echo Building $dir
+	cp -r ipbus/firmware/example_designs/projects/ise14_3 ./work_$dir
+	cd work_$dir
+	ln -s .. firmware
+	chmod u+x setup.sh
+	./setup.sh
+	xtclsh build_project.tcl
 done
 
