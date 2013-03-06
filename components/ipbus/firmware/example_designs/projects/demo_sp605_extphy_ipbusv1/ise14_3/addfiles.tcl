e6af852ce49a6deb3f52b749f9fc14b45a0c59a1
Dave Newbold <Dave.Newbold@cern.ch>
2013-03-06 15:22:36 +0000

    Adding build scripts     [CACTUS SVN 20084]

--- components/ipbus/firmware/example_designs/projects/demo_sp605_extphy_ipbusv1/ise14_3/addfiles.tcl
+++ components/ipbus/firmware/example_designs/projects/demo_sp605_extphy_ipbusv1/ise14_3/addfiles.tcl
@@ -34,17 +34,6 @@ proc addcore {file} {
 	eval addfile ipcore_dir/$bname
 }
 
-project new demo_atlys
-project set family spartan6
-project set device xc6slx45
-project set package csg324
-project set speed -2
-
-project set "Enable Multi-Threading" "2" -process "Map"
-project set "Pack I/O Registers/Latches into IOBs" "For Inputs and Outputs" -process "Map"
-project set "Enable Multi-Threading" "2" -process "Place & Route"
-project set "Enable BitStream Compression" TRUE -process "Generate Programming File"
-
 dofile file_list
 
 project close
