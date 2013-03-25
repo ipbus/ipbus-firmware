0f7d194882ebfe0157ca70e118498529e17a2446
Dave Newbold <Dave.Newbold@cern.ch>
2013-03-25 11:02:27 +0000

    Updating sim build scripts     [CACTUS SVN 20298]

--- components/ipbus/firmware/sim/scripts/setup.sh
+++ components/ipbus/firmware/sim/scripts/setup.sh
@@ -3,7 +3,7 @@
 vsim -c -do $REPOS_BUILD_DIR/setup_project.tcl
 cp -r $REPOS_FW_DIR/ipbus/firmware/ethernet/sim/modelsim_fli ./
 cd modelsim_fli
-. modelsim_fli/mac_fli_compile.sh
+. mac_fli_compile.sh
 cd ..
 ln -s modelsim_fli/mac_fli.so
 
