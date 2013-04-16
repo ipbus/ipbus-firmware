41eaf202f8afa87f462606a1c2a43b0d4b43f992
Dave Newbold <Dave.Newbold@cern.ch>
2013-04-16 14:06:17 +0000

    Fix     [CACTUS SVN 20486]

--- components/ipbus/firmware/ethernet/hdl/eth_7s_1000basex.vhd
+++ components/ipbus/firmware/ethernet/hdl/eth_7s_1000basex.vhd
@@ -151,7 +151,6 @@ begin
 			userclk2 => clk125,
 			independent_clock_bufg => fr_clk,
 			pma_reset => rsti,
-			mmcm_reset => mmcm_reset_phy,
 			mmcm_locked => mmcm_locked,
 			gmii_txd => gmii_txd,
 			gmii_tx_en => gmii_tx_en,
