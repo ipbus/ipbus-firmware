c9b4b98a4515411c684b7f9d60e44a09248e30c8
Dave Newbold <Dave.Newbold@cern.ch>
2013-04-25 14:24:24 +0000

    Adding minit     [CACTUS SVN 20622]

--- boards/mini_t/ipbus_test/minit_base/minit_240_ipb/ise14/setup_project.tcl
+++ boards/mini_t/ipbus_test/minit_base/minit_240_ipb/ise14/setup_project.tcl
@@ -4,6 +4,7 @@ project set device xc5vtx240t
 project set package ff1759
 project set speed -2
 
+project set "Other XST Command Line Options" "-use_new_parser yes" -process "Synthesize - XST"
 project set "Enable Multi-Threading" "2" -process "Map"
 project set "Pack I/O Registers/Latches into IOBs" "For Inputs and Outputs" -process "Map"
 project set "Enable Multi-Threading" "2" -process "Place & Route"
