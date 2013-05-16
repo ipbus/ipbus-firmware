68487805c13538657f35e9bfa3bf8246629c613a
Dave Newbold <Dave.Newbold@cern.ch>
2013-05-16 15:00:54 +0000

    Adding k7 basex project     [CACTUS SVN 20809]

--- components/ipbus/firmware/ethernet/gen_hdl/gig_eth_pcs_pma_v11_5/gig_eth_pcs_pma_v11_5_transceiver.vhd
+++ components/ipbus/firmware/ethernet/gen_hdl/gig_eth_pcs_pma_v11_5/gig_eth_pcs_pma_v11_5_transceiver.vhd
@@ -154,6 +154,8 @@ architecture wrapper of gig_eth_pcs_pma_v11_5_transceiver is
 
     SYSCLK_IN                               : in   std_logic;
     SOFT_RESET_IN                           : in   std_logic;
+-- DMN_CHANGE
+	DONT_RESET_ON_DATA_ERROR_IN: in std_logic;
     GT0_TX_FSM_RESET_DONE_OUT               : out  std_logic;
     GT0_RX_FSM_RESET_DONE_OUT               : out  std_logic;
     GT0_DATA_VALID_IN                       : in   std_logic;
@@ -570,6 +572,8 @@ begin
 
         SYSCLK_IN                       =>      independent_clock,
         SOFT_RESET_IN                   =>      pmareset,
+-- DMN_CHANGE
+		DONT_RESET_ON_DATA_ERROR_IN => '0',
         GT0_TX_FSM_RESET_DONE_OUT       =>      open,
         GT0_RX_FSM_RESET_DONE_OUT       =>      open,
     GT0_DATA_VALID_IN                   => data_valid_reg2,
