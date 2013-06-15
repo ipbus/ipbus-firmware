7f95393bd1100cebdc72260f9ee321ecb31f972f
Dave Newbold <Dave.Newbold@cern.ch>
2013-06-15 09:46:06 +0000

    Bug fix     [CACTUS SVN 21117]

--- components/ipbus/firmware/slaves/hdl/trans_buffer.vhd
+++ components/ipbus/firmware/slaves/hdl/trans_buffer.vhd
@@ -100,7 +100,7 @@ begin
 	begin
 		if rising_edge(clk_ipb) then
 			rdy <= (rdy or (mode_ipb and not mode_ipb_d)) and not t_in.pkt_done and mode_ipb;
-			done <= (done or t_in.pkt_done) and not mode_ipb;
+			done <= (done or t_in.pkt_done) and not (mode_ipb and not mode_ipb_d);
 		end if;
 	end process;
 	
