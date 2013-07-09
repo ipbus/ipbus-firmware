95627abfa6d709eed5777c91d577f2a92fe0c872
Dave Newbold <Dave.Newbold@cern.ch>
2013-07-09 15:52:02 +0000

    New example design address table     [CACTUS SVN 21570]

--- components/ipbus/firmware/example_designs/hdl/ipbus_addr_decode.vhd
+++ components/ipbus/firmware/example_designs/hdl/ipbus_addr_decode.vhd
@@ -19,20 +19,30 @@ package ipbus_addr_decode is
 end ipbus_addr_decode;
 
 package body ipbus_addr_decode is
-
+  
   function ipbus_addr_sel(signal addr : in std_logic_vector(31 downto 0)) return integer is
     variable sel : integer;
   begin
-		if    std_match(addr, "-------------------0----------0-") then
-			sel := 0; -- ctrl_reg / base 00000000 / mask 00000001
-		elsif std_match(addr, "-------------------0----------1-") then
-			sel := 1; -- reg / base 00000002 / mask 00000000
-		elsif std_match(addr, "-------------------1------------") then
-			sel := 2; -- ram / base 00001000 / mask 00000fff
-		else
-			sel := 99;
-		end if;
-		return sel;
-	end ipbus_addr_sel;
- 
+                -- START automatically  generated VHDL the Tue Jul  9 16:49:55 2013 
+		if    std_match(addr, "------------------00--------000-") then
+			sel := 0; -- ctrl_reg / base 0x00000000 / mask 0x0000300e
+		elsif std_match(addr, "------------------00--------001-") then
+			sel := 1; -- reg / base 0x00000002 / mask 0x0000300e
+		elsif std_match(addr, "------------------00--------01--") then
+			sel := 2; -- err_inject / base 0x00000004 / mask 0x0000300e
+		elsif std_match(addr, "------------------00--------100-") then
+			sel := 3; -- pkt_ctr / base 0x00000008 / mask 0x0000300e
+		elsif std_match(addr, "------------------01------------") then
+			sel := 4; -- ram / base 0x00001000 / mask 0x0000300e
+		elsif std_match(addr, "------------------10--------000-") then
+			sel := 5; -- pram / base 0x00002000 / mask 0x0000300e
+		-- END automatic generated VHDL
+
+                else
+                        sel := 99;
+                end if;
+                return sel;
+  end ipbus_addr_sel;
+                
 end ipbus_addr_decode;
+
