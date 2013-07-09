bc168b028ae06293bb4853a37f038b3e29a7df20
Dave Newbold <Dave.Newbold@cern.ch>
2013-07-09 15:46:46 +0000

    Adding extra entities to test design     [CACTUS SVN 21567]

--- components/ipbus/firmware/example_designs/hdl/slaves.vhd
+++ components/ipbus/firmware/example_designs/hdl/slaves.vhd
@@ -16,14 +16,16 @@ entity slaves is
 		ipb_out: out ipb_rbus;
 		rst_out: out std_logic;
 		eth_err_ctrl: out std_logic_vector(35 downto 0);
-		eth_err_stat: in std_logic_vector(47 downto 0) := X"000000000000"
+		eth_err_stat: in std_logic_vector(47 downto 0) := X"000000000000";
+		pkt_rx: in std_logic;
+		pkt_tx: in std_logic;
 	);
 
 end slaves;
 
 architecture rtl of slaves is
 
-	constant NSLV: positive := 4;
+	constant NSLV: positive := 6;
 	signal ipbw: ipb_wbus_array(NSLV-1 downto 0);
 	signal ipbr, ipbr_d: ipb_rbus_array(NSLV-1 downto 0);
 	signal ctrl_reg: std_logic_vector(31 downto 0);
@@ -97,5 +99,28 @@ begin
 		
 	eth_err_ctrl <= inj_ctrl(49 downto 32) & inj_ctrl(17 downto 0);
 	inj_stat <= X"00" & eth_err_stat(47 downto 24) & X"00" & eth_err_stat(23 downto 0);
-			
+	
+-- Slave 4: peephole RAM
+
+	slave4: entity work.ipbus_peephole_ram
+		generic map(addr_width => 10)
+		port map(
+			clk => ipb_clk,
+			reset => ipb_rst,
+			ipbus_in => ipbw(4),
+			ipbus_out => ipbr(4)
+		);
+		
+-- Slave 5: packet counters
+
+	slave5: entity work.ipbus_pkt_ctr
+		port map(
+			clk => ipb_clk,
+			reset => ipb_rst,
+			ipbus_in => ipbw(5),
+			ipbus_out => ipbr(5),
+			pkt_rx => pkt_rx,
+			pkt_tx => pkt_tx
+		);
+	
 end rtl;
