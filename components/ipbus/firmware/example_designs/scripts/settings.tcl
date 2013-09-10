7b30ad3d5a2331729845da769f62a3db3d1807bb
Dave Newbold <Dave.Newbold@cern.ch>
2013-09-10 16:00:17 +0000

    Adding standard ISE settings to scripts     [CACTUS SVN 23004]

---
+++ components/ipbus/firmware/example_designs/scripts/settings.tcl
@@ -0,0 +1,5 @@
+project set "Enable Multi-Threading" "2" -process "Map"
+project set "Pack I/O Registers/Latches into IOBs" "For Inputs and Outputs" -process "Map"
+project set "Enable Multi-Threading" "2" -process "Place & Route"
+project set "Enable BitStream Compression" TRUE -process "Generate Programming File"
+project set "Power Down Device if Over Safe Temperature" TRUE -process "Generate Programming File"
