40d1ae0dc660b26e8d464efef3c41e3de5afcebf
Dave Newbold <Dave.Newbold@cern.ch>
2013-09-20 13:03:49 +0000

    Adding stb output to ctrlreg     [CACTUS SVN 23217]

--- components/ipbus/firmware/slaves/hdl/ipbus_ctrlreg_v.vhd
+++ components/ipbus/firmware/slaves/hdl/ipbus_ctrlreg_v.vhd
@@ -26,7 +26,8 @@ entity ipbus_ctrlreg_v is
 		ipbus_in: in ipb_wbus;
 		ipbus_out: out ipb_rbus;
 		d: in ipb_reg_v(N_STAT - 1 downto 0);
-		q: out ipb_reg_v(N_CTRL - 1 downto 0)
+		q: out ipb_reg_v(N_CTRL - 1 downto 0);
+		stb: out std_logic(N_CTRL - 1 downto 0)
 	);
 	
 end ipbus_ctrlreg_v;
@@ -38,23 +39,28 @@ architecture rtl of ipbus_ctrlreg_v is
 	signal sel: integer := 0;
 	signal reg: ipb_reg_v(N_CTRL - 1 downto 0);
 	signal si, ri: ipb_reg_v(2 ** ADDR_WIDTH - 1 downto 0);
-	signal stat_cyc: std_logic;
+	signal stat_cyc, cw_cyc: std_logic;
 
 begin
 
 	sel <= to_integer(unsigned(ipbus_in.ipb_addr(ADDR_WIDTH - 1 downto 0))) when ADDR_WIDTH > 0 else 0;
 	stat_cyc <= ipbus_in.ipb_addr(ADDR_WIDTH);
+	cw_cyc <= ipbus_in.ipb_strobe and ipbus_in.ipb_write and not stat_cyc;
 
 	process(clk)
 	begin
 		if rising_edge(clk) then
 			if reset = '1' then
 				reg <= (others => (others => '0'));
-			elsif ipbus_in.ipb_strobe = '1' and ipbus_in.ipb_write = '1' and stat_cyc = '0' and sel < N_CTRL then
+			elsif cw_cyc = '1' and sel < N_CTRL then
 				reg(sel) <= ipbus_in.ipb_wdata;
 			end if;
 		end if;
 	end process;
+	
+	stb_gen: for i in N_CTRL - 1 downto 0 generate
+		stb(i) <= '1' when cw_cyc = '1' and sel = i else '0';
+	end generate;
 	
 	si(N_STAT - 1 downto 0) <= d;
 	si(2 ** ADDR_WIDTH - 1 downto N_STAT) <= (others => (others => '0'));
