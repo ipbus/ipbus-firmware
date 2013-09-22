1cc6ef87b4b61a7a75911d86de9e9080e8b23141
Dave Newbold <Dave.Newbold@cern.ch>
2013-09-22 21:36:59 +0000

    Fix     [CACTUS SVN 23275]

--- components/ipbus/firmware/slaves/hdl/ipbus_drp_bridge.vhd
+++ components/ipbus/firmware/slaves/hdl/ipbus_drp_bridge.vhd
@@ -44,8 +44,8 @@ begin
 	drp_out.data <= ipb_in.ipb_wdata(15 downto 0);
 	drp_out.we <= cyc and ipb_in.ipb_write;
 
-	ipb_out.ack <= drp_in.rdy and stb;
-	ipb_out.err <= '0';
+	ipb_out.ipb_ack <= drp_in.rdy and stb;
+	ipb_out.ipb_err <= '0';
 	ipb_out.ipb_rdata <= X"0000" & drp_in.data;
 
 end rtl;
