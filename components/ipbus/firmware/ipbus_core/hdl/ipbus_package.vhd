98070c06e0d2b638bdf40c47c812d3896cbafe42
Gregory Michiel Iles <gregory.iles@cern.ch>
2013-10-09 11:53:16 +0000

    updated interface     [CACTUS SVN 23647]

--- components/ipbus/firmware/ipbus_core/hdl/ipbus_package.vhd
+++ components/ipbus/firmware/ipbus_core/hdl/ipbus_package.vhd
@@ -25,7 +25,8 @@ package ipbus is
 	type ipb_rbus_array is array(natural range <>) of ipb_rbus;
 	
 	constant IPB_RBUS_NULL: ipb_rbus := ((others => '0'), '0', '0');
-	
+	constant IPB_WBUS_NULL: ipb_wbus := ((others => '0'), (others => '0'), '0', '0');
+
 -- For top-level generics
 	
 	type ipb_mac_cfg is (STATIC, CFG_SPACE);
