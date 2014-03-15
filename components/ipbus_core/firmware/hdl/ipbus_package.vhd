73aaa0891957ef0c9a9ccc403a2605ea1d6b6dee
Dave Newbold <Dave.Newbold@cern.ch>
2014-03-15 19:08:37 +0000

    Adding readout buffer some more     [CACTUS SVN 27803]

--- components/ipbus_core/firmware/hdl/ipbus_package.vhd
+++ components/ipbus_core/firmware/hdl/ipbus_package.vhd
@@ -5,16 +5,16 @@ package ipbus is
 
 -- The signals going from master to slaves
 	type ipb_wbus is
-  		record
-  			ipb_addr: std_logic_vector(31 downto 0);
-      	ipb_wdata: std_logic_vector(31 downto 0);
-      	ipb_strobe: std_logic;
-      	ipb_write: std_logic;
-    	end record;
+		record
+			ipb_addr: std_logic_vector(31 downto 0);
+			ipb_wdata: std_logic_vector(31 downto 0);
+			ipb_strobe: std_logic;
+			ipb_write: std_logic;
+		end record;
 
 	type ipb_wbus_array is array(natural range <>) of ipb_wbus;
-	 
--- The signals going from slaves to master	 
+
+-- The signals going from slaves to master
 	type ipb_rbus is
     record
 			ipb_rdata: std_logic_vector(31 downto 0);
