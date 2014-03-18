503dfc756e6be69eacc96a27e5381d007807e747
Andrew William Rose <andrew.william.rose@cern.ch>
2014-03-18 16:20:27 +0000

    [CACTUS SVN 27866]

--- components/ipbus_slaves/firmware/hdl/uc_pipe_interface.vhd
+++ components/ipbus_slaves/firmware/hdl/uc_pipe_interface.vhd
@@ -31,7 +31,7 @@ SIGNAL samp_nrd : std_logic_vector(1 DOWNTO 0) := (OTHERS => '1');
   signal clk200buf: std_logic;
 
       signal we_pipe : std_logic_vector(0 downto 0);
-      signal w_addr_pipe : unsigned(9 downto 0) := (others=>'0');
+      signal w_addr_pipe : unsigned(9 downto 0) := (others=>'1');
       signal w_data_pipe : std_logic_vector(15 downto 0);
       signal r_addr_ipbus : unsigned(8 downto 0) := (others=>'0');
       signal r_data_ipbus : std_logic_vector(31 downto 0);
@@ -41,6 +41,7 @@ SIGNAL samp_nrd : std_logic_vector(1 DOWNTO 0) := (OTHERS => '1');
       signal r_addr_pipe : unsigned(9 downto 0) := (others=>'0');
       signal r_data_pipe : std_logic_vector(15 downto 0);
 
+--       signal pipe_to_ipbus_reset , ipbus_to_pipe_reset : std_logic;
 
 
   COMPONENT sdpram_16x10_32x9
@@ -76,126 +77,115 @@ begin
     o => clk200buf
   );
 
-  uc_pipe <= r_data_pipe;
+
+--   w_data_pipe <= uc_pipe;
+--   uc_pipe <= r_data_pipe when uc_pipe_nwe = '1' else (OTHERS => 'Z');
 
 
-  process( clk200buf )
+  process( clk200buf , ipbus_to_pipe_reset , pipe_to_ipbus_reset )
   begin
-    if rising_edge( clk200buf ) then
+--     if( ipbus_to_pipe_reset = '1' ) then
+--       r_addr_pipe <= (others=>'0');
+--     elsif( pipe_to_ipbus_reset = '1' ) then
+--       w_addr_pipe <= (others=>'1');
+--     els
+    if  rising_edge( clk200buf ) then
 
       samp_nwe( 1 downto 0 ) <= samp_nwe(0) & uc_pipe_nwe;
       samp_nrd( 1 downto 0 ) <= samp_nrd(0) & uc_pipe_nrd;
+
+      we_pipe <= "0";
+
+      if( uc_pipe_nwe = '1' ) then
+        uc_pipe <= r_data_pipe;
+      else
+        uc_pipe <= (OTHERS => 'Z');
+      end if;
 
       if( samp_nrd = "10" ) then
         r_addr_pipe <= r_addr_pipe + 1;
       end if;
 
-    end if;
+      if( samp_nwe = "10" ) then
+        w_addr_pipe <= w_addr_pipe + 1;
+        we_pipe <= "1";
+      end if;
+
+--     end if;
   end process;
 
   
 
 
-
---   process( clk125 )
---   begin
--- -- 
--- --       samp_nrd <= samp_nrd(0) & uc_pipe_nrd;
--- 
---     if rising_edge( clk125 ) then
--- 
---       previous_samp_nwe <= samp_nwe;
---       previous_samp_nrd <= samp_nrd;
--- 
--- 
---       we_pipe <= "0";
--- -- 
--- --       -- NWE processing
--- -- --       IF done_nwe = '0' and (samp_nwe = "00") THEN
--- -- --         done_nwe <= '1';
--- -- --         we_pipe <= "1";
--- -- --         w_addr_pipe <= w_addr_pipe + 1;
--- -- --         -- The data bus should have settled a few clocks ago
--- -- --         -- If should be safe to directly sample it
--- -- --         w_data_pipe <= uc_pipe;
--- -- --         uc_pipe <= (OTHERS => 'Z');
--- -- --       ELSIF (samp_nwe = "11") THEN
--- -- --         -- NWE is stable high again, reset book-keeping
--- -- --         done_nwe <= '0';
---         uc_pipe <= r_data_pipe;
--- -- --       END IF;
--- --   
--- --       -- NRD processing
--- -- --       IF done_nrd = '0' and (samp_nrd = "00") THEN
--- -- --         done_nrd <= '1';
--- -- --         r_addr_pipe <= r_addr_pipe + 1;
--- -- --       ELSIF (samp_nrd = "11") THEN
--- -- --         done_nrd <= '0';
--- -- --       END IF;      
--- -- 
--- --     else
--- -- 
--- --       --Falling Edge
---       if( (samp_nrd = "10") or ( previous_samp_nrd(0) = '1' and samp_nrd(1) = '0'  ) ) then
---         r_addr_pipe <= r_addr_pipe + 1;
---       end if;
--- 
---     end if; --if rising_edge( clk125 )
---   end process;
-
-
-
-
-
   ipbus_out.ipb_err <= '0'; 
   ipbus_out.ipb_ack <= ack;
 
-  process(clk)
+  process(clk )
   begin
+
     if rising_edge( clk ) then 
 
-      ack <= ipbus_in.ipb_strobe and not ack;
-
-
-      --STATUS/CONFIGURATION REGISTER
-      if ipbus_in.ipb_strobe='1' and ack='0' and ipbus_in.ipb_write='0' then 
-        if ipbus_in.ipb_addr(1 downto 0)="00" then
-          ipbus_out.ipb_rdata <= "0000000" & std_logic_vector( w_addr_ipbus ) & "000000" & std_logic_vector( r_addr_pipe );
-        elsif ipbus_in.ipb_addr(1 downto 0)="01" then
-          ipbus_out.ipb_rdata <= "0000000" & std_logic_vector( r_addr_ipbus ) & "000000" & std_logic_vector( w_addr_pipe );
+--       if( ipbus_to_pipe_reset = '1' ) then
+--         ipbus_to_pipe_reset <= '0';  
+--         w_addr_ipbus <= (others=>'1');
+--       elsif( pipe_to_ipbus_reset = '1' ) then
+--         pipe_to_ipbus_reset <= '0';
+--         r_addr_ipbus <= (others=>'0');
+--       else
+  
+        ack <= ipbus_in.ipb_strobe and not ack;
+  
+        --STATUS/CONFIGURATION REGISTER
+        if ipbus_in.ipb_strobe='1' and ack='0' then
+          if ipbus_in.ipb_write='0' then 
+            if ipbus_in.ipb_addr(1 downto 0)="00" then
+              ipbus_out.ipb_rdata <= "0000000" & std_logic_vector( w_addr_ipbus ) & "000000" & std_logic_vector( r_addr_pipe );
+            elsif ipbus_in.ipb_addr(1 downto 0)="01" then
+              ipbus_out.ipb_rdata <= "0000000" & std_logic_vector( r_addr_ipbus ) & "000000" & std_logic_vector( w_addr_pipe );
+            end if;
+--           else
+--             if ipbus_in.ipb_addr(1 downto 0)="00" then
+--               ipbus_to_pipe_reset <= '1';          
+--             elsif ipbus_in.ipb_addr(1 downto 0)="01" then
+--               pipe_to_ipbus_reset <= '1';          
+--             end if;
+          end if;
         end if;
-      end if;
+  
+  
+        we_ipbus <= "0";
+        
+        --FIFO
+        if ipbus_in.ipb_strobe='1' and ack='0' and ipbus_in.ipb_addr(1 downto 0)="10" then
+        
+          if ipbus_in.ipb_write='1' then
+            w_data_ipbus <= ipbus_in.ipb_wdata( 15 downto 0 ) & ipbus_in.ipb_wdata( 31 downto 16 );
+            w_addr_ipbus <= w_addr_ipbus + 1;
+            we_ipbus <= "1";
+          else
+            ipbus_out.ipb_rdata <= r_data_ipbus( 15 downto 0 ) & r_data_ipbus( 31 downto 16 ) ;
+            r_addr_ipbus <= r_addr_ipbus + 1;
+          end if;
 
-
-      we_ipbus <= "0";
-      
-      --FIFO
-      if ipbus_in.ipb_strobe='1' and ack='0' and ipbus_in.ipb_addr(1 downto 0)="10" then
-      
-        if ipbus_in.ipb_write='1' then
-          w_data_ipbus <= ipbus_in.ipb_wdata( 15 downto 0 ) & ipbus_in.ipb_wdata( 31 downto 16 );
-          w_addr_ipbus <= w_addr_ipbus + 1;
-          we_ipbus <= "1";
-        else
-          ipbus_out.ipb_rdata <= r_data_ipbus( 15 downto 0 ) & r_data_ipbus( 31 downto 16 ) ;
-          r_addr_ipbus <= r_addr_ipbus + 1;
         end if;
-      end if;
+
+--       end if;
     end if; --if rising_edge( clk )
   end process;
 
 
 
---   ram_pipe_to_ipbus: sdpram_16x10_32x9
---     port map(
---       clka => clk200buf,
---       wea => we_pipe,
---       addra => w_addr_pipe,
---       dina => w_data_pipe,
---       clkb => clk,
---       addrb => std_logic_vector( r_addr_ipbus ),
---       doutb => r_data_ipbus
---     );
+
+  ram_pipe_to_ipbus: sdpram_16x10_32x9
+    port map(
+      clka => clk200buf,
+      wea => we_pipe,
+      addra => std_logic_vector( w_addr_pipe ),
+      dina => uc_pipe, --w_data_pipe,
+      clkb => clk,
+      addrb => std_logic_vector( r_addr_ipbus ),
+      doutb => r_data_ipbus
+    );
 
 
    ram_ipbus_to_pipe: sdpram_32x9_16x10
