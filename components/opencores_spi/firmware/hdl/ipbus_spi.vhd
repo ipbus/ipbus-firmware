1b9cdc21b787c25349b86c9dcea69e1e3624d1fe
Dave Newbold <Dave.Newbold@cern.ch>
2015-07-31 22:36:57 +0000

    References #TRAC-1321: Adding opencores_spi component     [CACTUS SVN 38078]

---
+++ components/opencores_spi/firmware/hdl/ipbus_spi.vhd
@@ -0,0 +1,60 @@
+-- ipbus_spi
+--
+-- Wrapper for opencores spi wishbone slave
+--
+-- http://opencores.org/project/spi
+--
+-- Dave Newbold, Jul 2015
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use work.ipbus.all;
+
+entity ipbus_spi is
+	port(
+		clk: in std_logic;
+		rst: in std_logic;
+		ipb_in: in ipb_wbus;
+		ipb_out: out ipb_rbus;
+		ss: out std_logic(7 downto 0);
+		mosi: out std_logic;
+		miso: in std_logic;
+		sclk: out std_logic
+	);
+	
+end ipbus_spi;
+
+architecture rtl of ipbus_spi is
+
+	signal stb, ack, err: std_logic;
+
+begin
+
+	stb <= ipb_in.ipb_strobe and not (ack or err);
+
+	spi: entity work.spi_top
+		port map(
+			wb_clk_i => clk,
+			wb_rst_i => rst,
+			wb_adr_i(4 downto 2) => ipb_in.ipb_addr(2 downto 0),
+			wb_adr_i(1 downto 0) => "00",
+			wb_dat_i => ipb_in.ipb_wdata,
+			wb_dat_o => ipb_out.ipb_rdata,
+			wb_sel_i => "1111",
+			wb_we_i => ipb_in.ipb_write,
+			wb_stb_i => stb,
+			wb_cyc_i => '1',
+			wb_ack_o => ack,
+			wb_err_o => err,
+			wb_int_o => open,
+			ss_pad_o => ss,
+			sclk_pad_o => sclk,
+			mosi_pad_o => mosi,
+			miso_pad_i => miso
+		);
+	
+	ipb_out.ipb_ack <= ack;
+	ipb_out.ipb_err <= err;
+	
+end rtl;
+
