7d29b795c12c312f1e12e82ec112704b0b7712a6
David Sankey <david.sankey@stfc.ac.uk>
2015-10-08 20:29:25 +0000

    Master to slave traffic     [CACTUS SVN 39922]

--- components/ipbus_core/firmware/hdl/udp_master_if.vhd
+++ components/ipbus_core/firmware/hdl/udp_master_if.vhd
@@ -36,3 +36,83 @@ ENTITY UDP_master_if IS
    );
 
 END UDP_master_if;
+
+architecture rtl of UDP_master_if is
+
+-- Inter FPGA protocol is as follows:
+-- a frame is defined from first valid data until last is asserted (i.e. no explicit BOF)
+-- Valid data are sent as data with kchar set to 0 (i.e. kchar 0 implies valid data)
+-- Valid data are put into a FIFO and if a kchar is sent the next tick then this is
+-- used to assert last and error (corresponding to tuser in the AXI MAC) as follows:
+-- K28.0  => last
+-- K28.2  => last and error
+-- If err is asserted during a frame error is automatically asserted at the end of the frame
+--
+-- Idle is indicated by K28.1 and K28.5
+-- By default K28.5 is used, but in the direction from slave to master K28.1 is used to signify 
+-- Got_IP_addr set to 0
+--        HGFEDCBA
+-- K28.0 "00011100"
+-- K28.1 "00111100"
+-- K28.2 "01011100"
+-- K28.5 "10111100"
+
+Signal my_rx_data: std_logic_vector(7 DOWNTO 0);
+Signal my_rx_error, my_rx_last, my_rx_valid, Got_IP_addr_sig: std_logic;
+
+Begin
+
+With Got_IP_addr_sig select my_rx_data <=
+  mac_rx_data when '1',
+  rarp_rx_data when Others;
+
+With Got_IP_addr_sig select my_rx_valid <=
+  mac_rx_valid when '1',
+  rarp_rx_valid when Others;
+
+With Got_IP_addr_sig select mac_rx_last <=
+  mac_rx_last when '1',
+  rarp_rx_last when Others;
+
+With Got_IP_addr_sig select my_rx_error <=
+  mac_rx_error when '1',
+  '0' when Others;
+
+rx_data_block:  process(mac_clk)
+  variable next_last, next_error, kchar, this_last, this_error: std_logic;
+  variable this_data: std_logic_vector(7 DOWNTO 0);
+  begin
+    If rising_edge(mac_clk) then
+      next_last := '0';
+      next_error := '0';
+      kchar := '1';
+      If rst_macclk = '1' then
+	this_data := "10111100";
+      ElsIf my_rx_valid = '1' then
+        this_data := my_rx_data;
+	next_last := my_rx_last;
+	next_error := my_rx_error;
+	kchar := '0';
+-- End of frame, send K28.0 or K28.2
+      ElsIf this_last = '1' then
+        this_data := "0" & this_error & "011100";
+-- Idle, send K28.5
+      Else
+        this_data := "10111100";
+      End If;
+      master_rx_data <= this_data
+-- pragma translate_off
+      after 4 ns
+-- pragma translate_on
+      ;
+      master_rx_kchar <= kchar
+-- pragma translate_off
+      after 4 ns
+-- pragma translate_on
+      ;
+      this_last := next_last;
+      this_error := next_error;
+    end if;
+  end process rx_data_block;
+
+End rtl;
--- components/ipbus_core/firmware/hdl/udp_slave_if.vhd
+++ components/ipbus_core/firmware/hdl/udp_slave_if.vhd
@@ -33,3 +33,82 @@ ENTITY UDP_slave_if IS
    );
 
 END UDP_slave_if;
+
+architecture rtl of UDP_slave_if is
+
+-- Inter FPGA protocol is as follows:
+-- a frame is defined from first valid data until last is asserted (i.e. no explicit BOF)
+-- Valid data are sent as data with kchar set to 0 (i.e. kchar 0 implies valid data)
+-- Valid data are put into a FIFO and if a kchar is sent the next tick then this is
+-- used to assert last and error (corresponding to tuser in the AXI MAC) as follows:
+-- K28.0  => last
+-- K28.2  => last and error
+-- If err is asserted during a frame error is automatically asserted at the end of the frame
+--
+-- Idle is indicated by K28.1 and K28.5
+-- By default K28.5 is used, but in the direction from slave to master K28.1 is used to signify 
+-- Got_IP_addr set to 0
+--        HGFEDCBA
+-- K28.0 "00011100"
+-- K28.1 "00111100"
+-- K28.2 "01011100"
+-- K28.5 "10111100"
+
+Begin
+
+rx_data_block:  process(mac_clk)
+  variable next_valid, this_valid, this_error, this_last, error_pending, frame: std_logic;
+  variable next_data, this_data: std_logic_vector(7 DOWNTO 0);
+  begin
+    If rising_edge(mac_clk) then
+      next_valid := '0';
+      this_last := '0';
+      this_error := '0';
+      If rst_macclk = '1' then
+        frame := '0';
+	this_valid := '0';
+	error_pending := '0';
+      ElsIf slave_rx_kchar = '0' then
+        next_valid := '1';
+	next_data := slave_rx_data;
+	frame := '1';
+-- K char.  Only K28.n have these bits not all set to 1 so only check for K28.0 and K28.2
+      ElsIf slave_rx_data(7) & slave_rx_data(5) = "00" then
+	this_last := '1';
+	this_error := slave_rx_data(6) or error_pending;  -- K28.2
+	frame := '0';
+	error_pending := '0';
+      End If;
+-- Capture transmission error during a packet.  This logic will ignore an 
+-- error flagged on  the K28.0 and K28.2 character, but that should be OK
+      If slave_rx_err = '1' then
+        error_pending := frame;
+      End If;
+      mac_rx_data <= this_data
+-- pragma translate_off
+      after 4 ns
+-- pragma translate_on
+      ;
+      mac_rx_error <= this_error
+-- pragma translate_off
+      after 4 ns
+-- pragma translate_on
+      ;
+      mac_rx_last <= this_last
+-- pragma translate_off
+      after 4 ns
+-- pragma translate_on
+      ;
+      mac_rx_valid <= this_valid
+-- pragma translate_off
+      after 4 ns
+-- pragma translate_on
+      ;
+      this_data := next_data;
+      this_valid := next_valid;
+    end if;
+  end process rx_data_block;
+
+
+
+End rtl;
