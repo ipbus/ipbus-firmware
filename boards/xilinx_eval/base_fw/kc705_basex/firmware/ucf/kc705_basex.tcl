0eb6e3005f6d254c2a4023b83a0872afa5698094
Dave Newbold <Dave.Newbold@cern.ch>
2015-12-18 11:08:02 +0000

    Updating TCL stuff     [CACTUS SVN 41231]

--- boards/xilinx_eval/base_fw/kc705_basex/firmware/ucf/kc705_basex.tcl
+++ boards/xilinx_eval/base_fw/kc705_basex/firmware/ucf/kc705_basex.tcl
@@ -2,7 +2,7 @@
 create_clock -period 8.000 -name eth_refclk [get_ports eth_clk_p]
 
 # Clock from Ethernet Transceiver (derived from Ethernet RefClk)
-create_clock -period 16.000 -name eth_transceiver [get_pins infra/eth/phy/*/gtxe2_i/TXOUTCLK]
+create_clock -period 16.000 -name eth_transceiver [get_pins -hier -filter {NAME=~"infra/eth/phy/*/gtxe2_i/TXOUTCLK"}]
 
 # The decoupled_clk is driven from a flip-flop to circumvent Xilinx rules for the ethernet sys clk.
 # i.e. sys clk must not be derived from eth refclk so that some monitoring can occur even with reclk failure.
@@ -16,8 +16,8 @@ create_generated_clock -name decoupled_clk -source [get_pins infra/eth/decoupled
 # Clocks derived from MMCM driven by Ethernet RefClk directly (i.e. not via transceiver)
 #create_generated_clock -name clk_ipb -source [get_pins infra/clocks/mmcm/CLKIN1] [get_pins infra/clocks/mmcm/CLKOUT1]
 
-set_false_path -through [get_pins infra/clocks/rst_reg/Q]
-set_false_path -through [get_nets infra/clocks/nuke_i]
+#set_false_path -through [get_pins infra/clocks/rst_reg/Q]
+#set_false_path -through [get_nets infra/clocks/nuke_i]
 
 set_property LOC GTXE2_CHANNEL_X0Y10 [get_cells -hier -filter {name=~infra/eth/*/gtxe2_i}]
 
