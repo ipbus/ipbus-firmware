e5037637a29ae5d20702cb1b424837a71e4003fc
Dave Newbold <Dave.Newbold@cern.ch>
2016-07-09 10:38:14 +0000

    Adding 18b ported RAM design     [assembla SVN 38]

--- components/ipbus_slaves/firmware/hdl/ipbus_dpram.vhd
+++ components/ipbus_slaves/firmware/hdl/ipbus_dpram.vhd
@@ -1,11 +1,11 @@
 -- ipbus_dpram
 --
--- Generic 32b wide dual-port memory with ipbus access on one port
+-- Generic 32b (or less) wide dual-port memory with ipbus access on one port
+-- This is a 'flat memory' mapping the RAM directly into ipbus address space
+-- Note that 1 wait state is required due to design of Xilinx block RAM
 --
 -- Should lead to an inferred block RAM in Xilinx parts with modern tools
 --
--- Note the wait state on ipbus access - full speed access is not possible
--- Can combine with peephole_ram access method for full speed access.
 --
 -- Dave Newbold, July 2013
 
@@ -16,7 +16,8 @@ use work.ipbus.all;
 
 entity ipbus_dpram is
 	generic(
-		ADDR_WIDTH: natural
+		ADDR_WIDTH: positive;
+		DATA_WIDTH: positive := 32
 	);
 	port(
 		clk: in std_logic;
@@ -25,8 +26,8 @@ entity ipbus_dpram is
 		ipb_out: out ipb_rbus;
 		rclk: in std_logic;
 		we: in std_logic := '0';
-		d: in std_logic_vector(31 downto 0) := (others => '0');
-		q: out std_logic_vector(31 downto 0);
+		d: in std_logic_vector(DATA_WIDTH - 1 downto 0) := (others => '0');
+		q: out std_logic_vector(DATA_WIDTH - 1 downto 0);
 		addr: in std_logic_vector(ADDR_WIDTH - 1 downto 0)
 	);
 	
@@ -34,7 +35,7 @@ end ipbus_dpram;
 
 architecture rtl of ipbus_dpram is
 
-	type ram_array is array(2 ** ADDR_WIDTH - 1 downto 0) of std_logic_vector(31 downto 0);
+	type ram_array is array(2 ** ADDR_WIDTH - 1 downto 0) of std_logic_vector(DATA_WIDTH - 1 downto 0);
 	shared variable ram: ram_array;
 	signal sel, rsel: integer range 0 to 2 ** ADDR_WIDTH - 1 := 0;
 	signal ack: std_logic;
@@ -46,9 +47,9 @@ begin
 	process(clk)
 	begin
 		if rising_edge(clk) then
-			ipb_out.ipb_rdata <= ram(sel); -- Order of statements is important to infer read-first RAM!
+			ipb_out.ipb_rdata <= (32 - DATA_WIDTH downto 0 => '0') & ram(sel); -- Order of statements is important to infer read-first RAM!
 			if ipb_in.ipb_strobe='1' and ipb_in.ipb_write='1' then
-				ram(sel) := ipb_in.ipb_wdata;
+				ram(sel) := ipb_in.ipb_wdata(DATA_WIDTH - 1 downto 0);
 			end if;
 			ack <= ipb_in.ipb_strobe and not ack;
 		end if;
---
+++ components/ipbus_slaves/firmware/hdl/ipbus_ported_dpram.vhd
@@ -0,0 +1,100 @@
+-- ipbus_dpram
+--
+-- Generic 32b (or less) wide dual-port memory with ported ipbus access on one side
+-- This is a 'ported ram' taking two ipbus addresses:
+-- word 0: r/w RAM pointer
+-- word 1: r/w data
+--
+-- Note that RAM access does not require a wait state, and the address auto-increments
+-- allowing efficient block transfers.
+--
+-- Should lead to an inferred block RAM in Xilinx parts with modern tools
+--
+-- Dave Newbold, October 2013
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+use work.ipbus.all;
+
+entity ipbus_ported_dpram is
+	generic(
+		ADDR_WIDTH: positive;
+		DATA_WIDTH: positive := 32
+	);
+	port(
+		clk: in std_logic;
+		rst: in std_logic;
+		ipb_in: in ipb_wbus;
+		ipb_out: out ipb_rbus;
+		rclk: in std_logic;
+		we: in std_logic := '0';
+		d: in std_logic_vector(DATA_WIDTH - 1 downto 0) := (others => '0');
+		q: out std_logic_vector(DATA_WIDTH - 1 downto 0);
+		addr: in std_logic_vector(ADDR_WIDTH - 1 downto 0)
+	);
+	
+end ipbus_ported_dpram;
+
+architecture rtl of ipbus_ported_dpram is
+
+	type ram_array is array(2 ** ADDR_WIDTH - 1 downto 0) of std_logic_vector(DATA_WIDTH - 1 downto 0);
+	shared variable ram: ram_array;
+	signal sel, rsel: integer range 0 to 2 ** ADDR_WIDTH - 1 := 0;
+	signal wcyc, wcyc_d: std_logic;
+	signal ptr: unsigned(ADDR_WIDTH - 1 downto 0);
+	signal data: std_logic_vector(DATA_WIDTH - 1 downto 0);
+	signal wea_l, wea_h: std_logic;
+
+begin
+
+	wcyc <= ipb_in.ipb_strobe and ipb_in.ipb_write;
+
+	process(clk)
+	begin
+		if falling_edge(clk) then
+			if rst = '1' then
+				ptr <= (others => '0');				
+			elsif ipb_in.ipb_addr(0) = '0' then
+				if wcyc = '1' then
+					ptr <= unsigned(ipb_in.ipb_wdata(ADDR_WIDTH downto 0));
+				end if;
+			else
+				if (ipb_in.ipb_strobe = '1' and ipb_in.ipb_write = '0') or (wcyc_d = '1') then
+					ptr <= ptr + 1;
+				end if;
+			end if;
+		end if;
+	end process;
+	
+	sel <= to_integer(ptr(ADDR_WIDTH downto 1));
+	
+	process(clk)
+	begin
+		if rising_edge(clk) then
+			data <= ram(sel);
+			if wcyc = '1' and ipb_in.ipb_addr(0) = '1' then
+				ram(sel) := ipb_in.ipb_wdata(DATA_WIDTH - 1 downto 0);
+			end if;
+			wcyc_d <= wcyc and ipb_in.ipb_addr(0);
+		end if;
+	end process;
+		
+	ipb_out.ipb_ack <= ipb_in.ipb_strobe;
+	ipb_out.ipb_err <= '0';
+	ipb_out.ipb_rdata <= (32 - ADDR_WIDTH downto 0 => '0') & std_logic_vector(ptr) when ipb_in.ipb_addr(0)='0'
+		else (32 - DATA_WIDTH downto 0 => '0') & data;
+	
+	rsel <= to_integer(unsigned(addr));
+	
+	process(rclk)
+	begin
+		if rising_edge(rclk) then
+			q <= ram(rsel); -- Order of statements is important to infer read-first RAM!
+			if we = '1' then
+				ram(rsel) := d;
+			end if;
+		end if;
+	end process;
+
+end rtl;
