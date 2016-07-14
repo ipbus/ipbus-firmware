affe4a6f62318abad09f155504466ebb3feb8ef3
Dave Newbold <Dave.Newbold@cern.ch>
2016-07-14 19:24:39 +0000

    Fix to ram     [assembla SVN 81]

--- components/ipbus_slaves/firmware/hdl/ipbus_ported_dpram.vhd
+++ components/ipbus_slaves/firmware/hdl/ipbus_ported_dpram.vhd
@@ -82,7 +82,7 @@ begin
 		
 	ipb_out.ipb_ack <= ipb_in.ipb_strobe;
 	ipb_out.ipb_err <= '0';
-	ipb_out.ipb_rdata <= (32 - ADDR_WIDTH downto 0 => '0') & std_logic_vector(ptr) when ipb_in.ipb_addr(0)='0'
+	ipb_out.ipb_rdata <= (31 - ADDR_WIDTH downto 0 => '0') & std_logic_vector(ptr) when ipb_in.ipb_addr(0)='0'
 		else (31 - DATA_WIDTH downto 0 => '0') & data;
 	
 	rsel <= to_integer(unsigned(addr));
