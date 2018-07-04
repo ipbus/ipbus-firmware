f6469f8a5b4cc099bfc32c2b67aa157d874529b8
Alessandro Thea <alessandro.thea@cern.ch>
2018-07-04 23:08:33 +0200

    Adding ipbus_extended_slaves entity. See #81

---
+++ components/ipbus_util/firmware/hdl/ipbus_extended_example.vhd
@@ -0,0 +1,126 @@
+---------------------------------------------------------------------------------
+--
+--   Copyright 2017 - Rutherford Appleton Laboratory and University of Bristol
+--
+--   Licensed under the Apache License, Version 2.0 (the "License");
+--   you may not use this file except in compliance with the License.
+--   You may obtain a copy of the License at
+--
+--       http://www.apache.org/licenses/LICENSE-2.0
+--
+--   Unless required by applicable law or agreed to in writing, software
+--   distributed under the License is distributed on an "AS IS" BASIS,
+--   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--   See the License for the specific language governing permissions and
+--   limitations under the License.
+--
+--                                     - - -
+--
+--   Additional information about ipbus-firmare and the list of ipbus-firmware
+--   contacts are available at
+--
+--       https://ipbus.web.cern.ch/ipbus
+--
+---------------------------------------------------------------------------------
+
+
+-- ipbus_example
+--
+-- selection of different IPBus slaves without actual function,
+-- just for performance evaluation of the IPbus/uhal system
+--
+-- Kristian Harder, March 2014
+-- based on code by Dave Newbold, February 2011
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use work.ipbus.all;
+use work.ipbus_reg_types.all;
+use work.ipbus_decode_ipbus_example.all;
+
+entity ipbus_example is
+	port(
+		ipb_clk: in std_logic;
+		ipb_rst: in std_logic;
+		ipb_in: in ipb_wbus;
+		ipb_out: out ipb_rbus;
+		nuke: out std_logic;
+		soft_rst: out std_logic;
+		userled: out std_logic
+	);
+
+end ipbus_example;
+
+architecture rtl of ipbus_example is
+
+	signal ipbw: ipb_wbus_array(N_SLAVES - 1 downto 0);
+	signal ipbr: ipb_rbus_array(N_SLAVES - 1 downto 0);
+	signal ctrl, stat: ipb_reg_v(0 downto 0);
+
+begin
+
+-- ipbus address decode
+		
+	fabric: entity work.ipbus_fabric_sel
+    generic map(
+    	NSLV => N_SLAVES,
+    	SEL_WIDTH => IPBUS_SEL_WIDTH)
+    port map(
+      ipb_in => ipb_in,
+      ipb_out => ipb_out,
+      sel => ipbus_sel_ipbus_example(ipb_in.ipb_addr),
+      ipb_to_slaves => ipbw,
+      ipb_from_slaves => ipbr
+    );
+
+-- Slave 0: id / rst reg
+
+	slave0: entity work.ipbus_ctrlreg_v
+		port map(
+			clk => ipb_clk,
+			reset => ipb_rst,
+			ipbus_in => ipbw(N_SLV_CSR),
+			ipbus_out => ipbr(N_SLV_CSR),
+			d => stat,
+			q => ctrl
+		);
+		
+		stat(0) <= X"abcdfedc";
+		soft_rst <= ctrl(0)(0);
+		nuke <= ctrl(0)(1);
+		userled <= ctrl(0)(2);
+
+-- Slave 1: register
+
+	slave1: entity work.ipbus_reg_v
+		port map(
+			clk => ipb_clk,
+			reset => ipb_rst,
+			ipbus_in => ipbw(N_SLV_REG),
+			ipbus_out => ipbr(N_SLV_REG),
+			q => open
+		);
+
+-- Slave 2: 1kword RAM
+
+	slave4: entity work.ipbus_ram
+		generic map(ADDR_WIDTH => 10)
+		port map(
+			clk => ipb_clk,
+			reset => ipb_rst,
+			ipbus_in => ipbw(N_SLV_RAM),
+			ipbus_out => ipbr(N_SLV_RAM)
+		);
+	
+-- Slave 3: peephole RAM
+
+	slave5: entity work.ipbus_peephole_ram
+		generic map(ADDR_WIDTH => 10)
+		port map(
+			clk => ipb_clk,
+			reset => ipb_rst,
+			ipbus_in => ipbw(N_SLV_PRAM),
+			ipbus_out => ipbr(N_SLV_PRAM)
+		);
+
+end rtl;
