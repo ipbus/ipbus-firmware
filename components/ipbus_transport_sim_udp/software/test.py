b2bd3535a7a323934e19740a8ce79d1d224a51dc
David Newbold dave.newbold@stfc.ac.uk <dmn@heplnw061.pp.rl.ac.uk>
2019-04-15 12:45:49 +0100

    Test script update. See #105

--- components/ipbus_transport_sim_udp/software/test.py
+++ components/ipbus_transport_sim_udp/software/test.py
@@ -3,10 +3,10 @@
 import uhal
 import time
 import sys
-from I2CuHal import I2CCore
 
 # uhal.setLogLevelTo(uhal.LogLevel.ERROR)
 hw = uhal.getDevice("sim", "ipbusudp-2.0://127.0.0.1:50001", "file://ipbus_example.xml")
 
 v = hw.getNode("reg").read()
+hw.dispatch()
 print hex(v)
