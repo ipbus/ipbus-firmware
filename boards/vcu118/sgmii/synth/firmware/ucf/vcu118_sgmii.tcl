f172425e80b590036b3fecf8c166f4e8a0dc1da6
Alessandro Thea <alessandro.thea@cern.ch>
2019-09-02 22:59:30 +0200

    Commented-out all set_false_path statements on resets, to check what designs actually need them. See #107

--- boards/vcu118/sgmii/synth/firmware/ucf/vcu118_sgmii.tcl
+++ boards/vcu118/sgmii/synth/firmware/ucf/vcu118_sgmii.tcl
@@ -97,9 +97,9 @@ create_generated_clock -name ipbus_clk -source [get_pins infra/clocks/mmcm/CLKIN
 set_clock_groups -asynchronous -group [ get_clocks -include_generated_clocks osc_clk_300 ] -group [ get_clocks -include_generated_clocks osc_clk_125 ]  -group [ get_clocks -include_generated_clocks sgmii_clk_p ] 
 
 
-set_false_path -from [get_pins infra/clocks/nuke_i_reg/C] -to [get_pins infra/clocks/nuke_d_reg/D]
-set_false_path -from [get_pins infra/clocks/rst_reg/C] -to [get_pins infra/clocks/rst_ipb_ctrl_reg/D]
-set_false_path -from [get_pins infra/clocks/rst_reg/C] -to [get_pins infra/clocks/rst_ipb_reg/D]
+# set_false_path -from [get_pins infra/clocks/nuke_i_reg/C] -to [get_pins infra/clocks/nuke_d_reg/D]
+# set_false_path -from [get_pins infra/clocks/rst_reg/C] -to [get_pins infra/clocks/rst_ipb_ctrl_reg/D]
+# set_false_path -from [get_pins infra/clocks/rst_reg/C] -to [get_pins infra/clocks/rst_ipb_reg/D]
 
 false_path {leds[*]} osc_clk_300
 false_path {rst_in[*]} osc_clk_125
