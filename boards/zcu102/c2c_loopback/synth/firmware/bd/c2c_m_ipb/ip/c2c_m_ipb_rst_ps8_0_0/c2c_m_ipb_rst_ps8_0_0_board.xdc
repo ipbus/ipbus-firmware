95992b6467ab30b5e57a65ab19d31a3c7d1368c4
Tom Williams <tom.williams@cern.ch>
2019-11-02 23:30:59 +0000

    ZCU102 C2C design: Remove generated files for c2c_m_ipb block design. See #133

--- boards/zcu102/c2c_loopback/synth/firmware/bd/c2c_m_ipb/ip/c2c_m_ipb_rst_ps8_0_0/c2c_m_ipb_rst_ps8_0_0_board.xdc
+++
@@ -1,3 +0,0 @@
-#--------------------Physical Constraints-----------------
-
-set_property BOARD_PIN {CPU_RESET} [get_ports ext_reset_in]
--- boards/zcu102/c2c_loopback/synth/firmware/cfg/add_block_designs.tcl
+++ boards/zcu102/c2c_loopback/synth/firmware/cfg/add_block_designs.tcl
@@ -2,4 +2,5 @@
 set script_path [ file dirname [ file normalize [ info script ] ] ]
 
 read_bd ${script_path}/../bd/c2c_s_ipb/c2c_s_ipb.bd
-read_bd ${script_path}/../bd/c2c_m_ipb/c2c_m_ipb.bd
+#read_bd ${script_path}/../bd/c2c_m_ipb/c2c_m_ipb.bd
+generate_target {synthesis implementation} [import_files ${script_path}/../bd/c2c_m_ipb/c2c_m_ipb.bd]
