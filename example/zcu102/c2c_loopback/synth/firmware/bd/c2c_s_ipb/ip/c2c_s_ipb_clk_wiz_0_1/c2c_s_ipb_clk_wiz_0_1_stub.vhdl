91aa87e16bc4d8f968c04390558f0ddc4642288b
Tom Williams <tom.williams@cern.ch>
2019-11-09 15:05:05 +0000

    ZCU102 AXI C2C design: Update to 2019.1.1. See #133

--- example/zcu102/c2c_loopback/synth/firmware/bd/c2c_s_ipb/ip/c2c_s_ipb_clk_wiz_0_1/c2c_s_ipb_clk_wiz_0_1_stub.vhdl
+++ example/zcu102/c2c_loopback/synth/firmware/bd/c2c_s_ipb/ip/c2c_s_ipb_clk_wiz_0_1/c2c_s_ipb_clk_wiz_0_1_stub.vhdl
@@ -1,10 +1,10 @@
 -- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2019.1 (lin64) Build 2552052 Fri May 24 14:47:09 MDT 2019
--- Date        : Sun Nov  3 16:31:22 2019
+-- Date        : Sat Nov  9 13:00:01 2019
 -- Host        : heplnw017.pp.rl.ac.uk running 64-bit CentOS Linux release 7.7.1908 (Core)
 -- Command     : write_vhdl -force -mode synth_stub
---               /data/tsw/firmware/ipbus-dev-zcu102-c2c/proj/zcu102_2019.1_onlyXCI/zcu102_2019.1_onlyXCI/zcu102_2019.1_onlyXCI.srcs/sources_1/bd/c2c_s_ipb/ip/c2c_s_ipb_clk_wiz_0_1/c2c_s_ipb_clk_wiz_0_1_stub.vhdl
+--               /data/tsw/firmware/ipbus-dev-zcu102-c2c/proj/zcu102_2019.1to2019.1.1/zcu102_2019.1to2019.1.1/zcu102_2019.1to2019.1.1.srcs/sources_1/bd/c2c_s_ipb/ip/c2c_s_ipb_clk_wiz_0_1/c2c_s_ipb_clk_wiz_0_1_stub.vhdl
 -- Design      : c2c_s_ipb_clk_wiz_0_1
 -- Purpose     : Stub declaration of top-level module interface
 -- Device      : xczu9eg-ffvb1156-2-e
