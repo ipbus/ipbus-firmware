cabe28eb37d6da9d4a6558d48589bff46cca3b47
David Sankey <David.Sankey@stfc.ac.uk>
2021-02-03 18:48:53 +0000

    Capture IP address from DHCPOFFER

--- components/ipbus_transport_udp/firmware/hdl/udp_ipaddr_ipam.vhd
+++ components/ipbus_transport_udp/firmware/hdl/udp_ipaddr_ipam.vhd
@@ -86,7 +86,8 @@ MAC_IP_addr_rx_vld_block:  process (mac_clk)
     end if;
   end process;
 
-MAC_IP_addr_rx_block: process(mac_clk)
+rarp_reply: if DHCP_RARP = '0' generate
+MAC_IP_addr_rx_rarp: process(mac_clk)
   variable pkt_mask: std_logic_vector(41 downto 0);
   variable MAC_IP_addr_rx_int: std_logic_vector(79 downto 0);
   begin
@@ -111,6 +112,35 @@ MAC_IP_addr_rx_block: process(mac_clk)
       ;
     end if;
   end process;
+end generate rarp_reply;
+
+dhcp_offer: if DHCP_RARP = '1' generate
+MAC_IP_addr_rx_dhcp: process(mac_clk)
+  variable pkt_mask: std_logic_vector(73 downto 0);
+  variable MAC_IP_addr_rx_int: std_logic_vector(79 downto 0);
+  begin
+    if rising_edge(mac_clk) then
+      if rx_reset = '1' then
+         pkt_mask := "111111" & "111111" & "1111" & "1111" & "1111" &
+		"11" & "1111" & "1111" & "1111" & "1111" & "1111" &
+		"111111" & "1111" & "0000" & "1111" & "1111" & "000000";
+	MAC_IP_addr_rx_int := (Others => '0');
+      elsif my_rx_valid = '1' then
+        if pkt_drop_ipam = '1' then
+	  MAC_IP_addr_rx_int := (Others => '0');
+        elsif pkt_mask(73) = '0' then
+          MAC_IP_addr_rx_int := MAC_IP_addr_rx_int(71 downto 0) & my_rx_data;
+        end if;
+        pkt_mask := pkt_mask(72 downto 0) & '1';
+      end if;
+      MAC_IP_addr_rx <= MAC_IP_addr_rx_int
+-- pragma translate_off
+      after 4 ns
+-- pragma translate_on
+      ;
+    end if;
+  end process;
+end generate dhcp_offer;
 
 My_MAC_IP_addr_block:  process (mac_clk)
   variable Got_MAC_IP_addr_rx, last_enable_125: std_logic;
@@ -121,18 +151,23 @@ My_MAC_IP_addr_block:  process (mac_clk)
       if (rst_macclk = '1') or
       (enable_125 = '1' and last_enable_125 = '0') then
         Got_MAC_IP_addr_rx := '0';
-	My_MAC_IP_addr_int := MAC_addr & IP_addr;
+	My_MAC_IP_addr_int := IP_addr & MAC_addr;
       elsif MAC_IP_addr_rx_vld = '1' and ipam_125 = '1' then
         Got_MAC_IP_addr_rx := '1';
-	My_MAC_IP_addr_int := MAC_IP_addr_rx;
+        if DHCP_RARP = '0'  then
+-- IP address and MAC address are swapped compared to DHCP...
+		  My_MAC_IP_addr_int := MAC_IP_addr_rx(31 downto 0) & MAC_IP_addr_rx(79 downto 32);
+		else
+		  My_MAC_IP_addr_int := MAC_IP_addr_rx;
+		end if;
       end if;
       last_enable_125 := enable_125;
-      My_MAC_addr <= My_MAC_IP_addr_int(79 downto 32)
+      My_IP_addr <= My_MAC_IP_addr_int(79 downto 48)
 -- pragma translate_off
       after 4 ns
 -- pragma translate_on
       ;
-      My_IP_addr <= My_MAC_IP_addr_int(31 downto 0)
+      My_MAC_addr <= My_MAC_IP_addr_int(47 downto 0)
 -- pragma translate_off
       after 4 ns
 -- pragma translate_on
