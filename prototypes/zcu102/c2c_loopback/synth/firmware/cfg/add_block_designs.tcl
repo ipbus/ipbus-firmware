33ace93a2ca9a04140f28ff165c8bc8be85e9733
Alessandro Thea <alessandro.thea@cern.ch>
2021-02-05 12:38:32 +0100

    Adding ip upgrade in the bd add block logic

--- prototypes/zcu102/c2c_loopback/synth/firmware/cfg/add_block_designs.tcl
+++ prototypes/zcu102/c2c_loopback/synth/firmware/cfg/add_block_designs.tcl
@@ -4,8 +4,17 @@ set script_path [ file dirname [ file normalize [ info script ] ] ]
 set bd_files [list ${script_path}/../bd/c2c_m_ipb/c2c_m_ipb.bd ${script_path}/../bd/c2c_s_ipb/c2c_s_ipb.bd]
 
 foreach file_path ${bd_files} {
-  set bd_file_path [import_files ${file_path}]
+  puts "${file_path}"
+  set bd_file_path [import_files -force ${file_path}]
   open_bd_design ${bd_file_path}
+
+
+  set bd_ips [get_ips [current_bd_design]_*]
+
+  foreach ip $bd_ips {
+    upgrade_ip [get_ips $ip]
+  }
+
   reset_target {synthesis implementation} [get_files ${bd_file_path}]
   generate_target {synthesis implementation} [get_files ${bd_file_path}]
 }
