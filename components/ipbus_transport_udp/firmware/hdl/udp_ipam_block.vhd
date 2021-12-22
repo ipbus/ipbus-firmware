939e4d318bc6d18d8e82864d40b18c619df2bacd
David Sankey <David.Sankey@stfc.ac.uk>
2021-12-22 12:37:32 +0000

    Rework dhcp_block

--- components/ipbus_transport_udp/firmware/hdl/udp_ipam_block.vhd
+++ components/ipbus_transport_udp/firmware/hdl/udp_ipam_block.vhd
@@ -80,7 +80,7 @@ dhcp_discover: if DHCP_RARP = '1' generate
     -- IP header length = x"45"
 -- IPV4
     -- 'Differentiated services' set to: x"00"
-    -- IPv4 Packet Length 285 = x"011D"
+    -- IPv4 Packet Length 272 (DHCPDISCOVER) or 284 (DHCPREQUEST) = x"0110" or x"011c"
     -- Identification: MAC address bits
     -- Flags: x"4000" (Don't fragment)
     -- Time to live ~64s x"40"
@@ -90,7 +90,7 @@ dhcp_discover: if DHCP_RARP = '1' generate
     -- Dest IP (broadcast on discover) 255.255.255.255 = x"FFFFFFFF"
 -- UDP
     -- Ports (68->67) x"00440043"
-    -- UDP Packet Length 265 =x"0109"
+    -- UDP Packet Length 252 or 264 = x"00fc" or x"0108"
     -- Checksum empty =x"0000"
 -- Bootstrap Discover Packet
     -- Boot request x"01"
@@ -105,60 +105,120 @@ dhcp_discover: if DHCP_RARP = '1' generate
     -- 192 empty bytes (!)
 -- DHCP magic cookie (x"63825363")
 -- Options
-    -- Discover option x"350101"
-    -- Max size (opt 57) opt size 2 msg size 576 x"39020240" 
-    -- Large Parameter list:
-        --Option: (55) Parameter Request List x"37077D014206034396"
-        --   Length: 7
-        --    Parameter Request List Item: (125) V-I Vendor-specific Information
-        --    Parameter Request List Item: (1) Subnet Mask
-        --    Parameter Request List Item: (66) TFTP Server Name
-        --    Parameter Request List Item: (6) Domain Name Server
-        --    Parameter Request List Item: (3) Router
-        --    Parameter Request List Item: (67) Bootfile name
-        --    Parameter Request List Item: (150) TFTP Server Address
--- end x"FF"
+    -- DHCPDISCOVER x"350101" or DHCPREQUEST x"350103"
+    -- DHCPREQUEST Server Identifier x"3604" & Server_IP_addr
+    -- DHCPREQUEST Requested IP Address x"3204" & My_IP_addr
+	-- End x"FF"
 dhcp_block:  process(mac_clk)
-  variable data_buffer: std_logic_vector(2383 downto 0):= (others => '0');
-  variable we_buffer: std_logic_vector(297 downto 0) := (others => '0');
-  constant zeroes_192: std_logic_vector(1535 downto 0) := (others => '0');
+  variable pkt_data: std_logic_vector(7 downto 0);
+  variable data_buffer: std_logic_vector(47 downto 0);
+  variable we: std_logic;
   begin
-    if rising_edge(mac_clk) then
-      if (rst_macclk = '1') then
-		we_buffer := (Others => '0');
-      elsif ipam_req = '1' then
-      	data_buffer(2383 downto 112)  :=  x"FFFFFFFFFFFF" & MAC_addr & x"0800" &x"45"   -- Ethernet header
-        & x"00" & x"011c" & MAC_addr(15 downto 0) & x"4000" & x"40" & x"11" & x"7929" & x"C0A8" & NOT MAC_addr(15 downto 0) & x"FFFFFFFF" -- IPv4 Header
-      --  & x"c0a800ef" & x"FFFFFFFF" crosscheck for sniped packetMSB_half_store
-        & x"00440043" &  x"0108" & x"0000" &  x"01" & x"01" & x"06" & x"00" & MAC_addr(31 downto 0) & std_logic_vector(secs_elapsed)
-        & x"8000" & x"00000000" & x"00000000"& x"00000000"& x"00000000" & MAC_addr & x"00000000000000000000" & zeroes_192  -- Bootstrap
-        & x"63825363" -- DHCP Magic Cookie
-        & x"3501";
-	  	we_buffer := (Others => '1');
-	  	if ipam_running = '1' then -- dhcp discover
-	  	  data_buffer(2247 downto 2240) := x"10";  -- IP length
-	  	  data_buffer(2183 downto 2176) := x"35";  -- IP checksum
-	  	  data_buffer(2079 downto 2064) := x"00fc";  -- UDP length
-	  	  data_buffer(111 downto 0) := x"01ff000000000000000000000000";
-	  	  we_buffer(11 downto 0) := (Others => '0');
-	  	else -- dhcp request
-	  	  data_buffer(111 downto 0) := x"033604" & Server_IP_addr & x"3204" & My_IP_addr & x"ff";
-	  	end if;
-      end if;
-      ipam_data <= data_buffer(2383 downto 2376)
+    If rising_edge(mac_clk) then
+      If rst_macclk = '1' then
+        we := '0';
+	  ElsIf ipam_req = '1' then
+-- ETHERNET
+    -- Ethernet DST_MAC(6) discover -> broadcast (FFFFFFFFFFFF) , 
+        we := '1';
+		data_buffer := x"FFFFFFFFFFFF";
+      End If;
+      Case to_integer(address) is
+	  When 5 | 69 =>
+	-- SRC_MAC(6) or Bootstrap Discover MAC address = MAC_addr
+		data_buffer := MAC_addr;
+	  When 11 =>
+    -- Ether_Type IPv4 = x"0800" 
+    -- IP header length = x"45"
+-- IPV4
+    -- 'Differentiated services' set to: x"00"
+    -- IPv4 Packet Length 272 (DHCPDISCOVER) or 284 (DHCPREQUEST) = x"0110" or x"011c"
+		data_buffer(43 downto 4) := x"8004500011";
+		if ipam_running = '0' then -- dhcp request
+		  data_buffer(3 downto 0) := x"c";
+		end if;
+	  When 17 =>
+    -- Identification: MAC address bits
+    -- Flags: x"4000" (Don't fragment)
+    -- Time to live ~64s x"40"
+    -- UDP protocol = x"11" (17)
+		data_buffer := MAC_addr(15 downto 0) & x"40004011";
+	  When 23 =>
+    -- Checksum: Fixed by source IP and ID 
+    -- Source IP (on discover) 0.0.0.0 =  x 192.168 & NOT MAC addr(15 downto 0)
+		if ipam_running = '1' then -- dhcp discover
+		  data_buffer(47 downto 32) := x"7935";
+		else
+		  data_buffer(47 downto 32) := x"7929";
+		end if;
+		data_buffer(31 downto 0) := x"C0A8" & NOT MAC_addr(15 downto 0);
+	  When 29 =>
+    -- Dest IP (broadcast on discover) 255.255.255.255 = x"FFFFFFFF"
+-- UDP
+    -- Source Port (68) x"0044"
+		data_buffer := x"FFFFFFFF0044";
+	  When 35 =>
+    -- Destination Port (67) x"0043"
+    -- UDP Packet Length 252 or 264 = x"00fc" or x"0108"
+    -- Checksum empty =x"0000" (default)
+		data_buffer(39 downto 32) := x"43";
+		if ipam_running = '1' then -- dhcp discover
+		  data_buffer(23 downto 16) := x"fc";
+		else
+		  data_buffer(27 downto 16) := x"108";
+		end if;
+	  When 41 =>
+-- Bootstrap Discover Packet
+    -- Boot request x"01"
+    -- Ethernet x"01"
+    -- (HW) Address length x"06" (in octets/bytes)
+    -- hops x"00"
+    -- trans ID  MAC_addr(31 downto 0) fudge as only 1 XID
+		data_buffer := x"01010600" & MAC_addr(31 downto 16);
+	  When 47 =>
+    -- secs elapsed sec_count defined through sec_counter
+    -- bootpflag  x"8000" broadcast
+		data_buffer(47 downto 12) := MAC_addr(15 downto 0) & std_logic_vector(secs_elapsed) & x"8";
+	  When 277 =>
+-- DHCP magic cookie (x"63825363")
+-- Options
+    -- DHCP Message type x"3501"
+		data_buffer := x"638253633501";
+	  When 283 =>
+		if ipam_running = '1' then -- dhcp discover and finish
+		  data_buffer(47 downto 32) := x"01ff";
+		else -- dhcp request, then server identifier
+		  data_buffer := x"033604" & Server_IP_addr(31 downto 8);
+		end if;
+	  When 285 =>
+		if ipam_running = '1' then -- EOP DHCP discover
+		  we := '0';
+		end if;
+	  When 289 => -- now only DHCP request...
+	-- server identifier (cont) and Requested IP Address
+	  	data_buffer := Server_IP_addr(7 downto 0) & x"3204" & My_IP_addr(31 downto 8);
+	  When 295 =>
+	-- Requested IP Address (cont) and finish
+	  	data_buffer(47 downto 32) := My_IP_addr(7 downto 0) & x"ff";
+	  When 297 =>
+		we := '0';
+	  When Others =>
+	  	NULL;
+      End Case;
+      pkt_data := data_buffer(47 downto 40);
+	  data_buffer := data_buffer(39 downto 0) & x"00";
+      ipam_data <= pkt_data
 -- pragma translate_off
       after 4 ns
 -- pragma translate_on
       ;
-      ipam_we_sig <= we_buffer(297)
+      ipam_we_sig <= we
 -- pragma translate_off
       after 4 ns
 -- pragma translate_on
       ;
-      data_buffer := data_buffer(2375 downto 0) & x"00";
-      we_buffer := we_buffer(296 downto 0) & '0';
     end if;
-  end process;
+  end process dhcp_block;
 
 delay_ipam_mode: process(mac_clk)
   variable ipam_mode_i: std_logic := '0';
