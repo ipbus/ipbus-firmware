b59980037225de164d8462a4bf450f6d0faee8a5
Adam Barcock <adam.barcock@stfc.ac.uk>
2022-03-21 16:52:25 +0000

    Design enables multiple GbE connections via 7 series GTPs on the same quad.

---
+++ components/ipbus_eth/firmware/hdl/eth_7s_1000basex_gtp_shared_logic_output.vhd
@@ -0,0 +1,255 @@
+---------------------------------------------------------------------------------
+--
+--   Copyright 2017 - Rutherford Appleton Laboratory and University of Bristol
+--
+--   Licensed under the Apache License, Version 2.0 (the "License");
+--   you may not use this file except in compliance with the License.
+--   You may obtain a copy of the License at
+--
+--       http://www.apache.org/licenses/LICENSE-2.0
+--
+--   Unless required by applicable law or agreed to in writing, software
+--   distributed under the License is distributed on an "AS IS" BASIS,
+--   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--   See the License for the specific language governing permissions and
+--   limitations under the License.
+--
+--                                     - - -
+--
+--   Additional information about ipbus-firmare and the list of ipbus-firmware
+--   contacts are available at
+--
+--       https://ipbus.web.cern.ch/ipbus
+--
+---------------------------------------------------------------------------------
+
+
+-- eth_7s_1000basex_gtp
+--
+-- Contains the instantiation of the Xilinx MAC & 1000baseX pcs/pma & GTP transceiver cores
+--
+-- This version is for the artix GTP transceivers, and has the GTPE2_COMMON included in this block.
+-- Various PLL clock outputs are therefore provided for use by other MGTs in the same quad.
+--
+-- Do not change signal names in here without corresponding alteration to the timing contraints file
+--
+-- Dave Newbold, January 2016
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+library unisim;
+use unisim.VComponents.all;
+
+entity eth_7s_1000basex_gtp_shared_logic_out is
+	port(
+		gt_clkp, gt_clkn: in std_logic;
+		gt_txp, gt_txn: out std_logic;
+		gt_rxp, gt_rxn: in std_logic;
+		sfp_los: in std_logic;
+		clk125_out: out std_logic;
+		clk125_fr: out std_logic;
+		rsti: in std_logic;
+		locked: out std_logic;
+		tx_data: in std_logic_vector(7 downto 0);
+		tx_valid: in std_logic;
+		tx_last: in std_logic;
+		tx_error: in std_logic;
+		tx_ready: out std_logic;
+		rx_data: out std_logic_vector(7 downto 0);
+		rx_valid: out std_logic;
+		rx_last: out std_logic;
+		rx_error: out std_logic;
+        pma_reset               : out std_logic;
+        gt0_pll0outclk_in       : out std_logic;
+        gt0_pll0outrefclk_in    : out std_logic;
+        gt0_pll1outclk_in       : out std_logic;
+        gt0_pll1outrefclk_in    : out std_logic;
+        gt0_pll0lock_in         : out std_logic;
+        gt0_pll0refclklost_in   : out std_logic;
+        user_clk                : out std_logic;
+        gtrefclk_out            : out std_logic;
+        mmcm_locked             : out std_logic;
+        clk_indep   : out std_logic 
+	);
+
+end eth_7s_1000basex_gtp_shared_logic_out;
+
+architecture rtl of eth_7s_1000basex_gtp_shared_logic_out is
+
+    COMPONENT temac_gbe_v9_0
+        PORT (
+            gtx_clk : IN STD_LOGIC;
+            glbl_rstn : IN STD_LOGIC;
+            rx_axi_rstn : IN STD_LOGIC;
+            tx_axi_rstn : IN STD_LOGIC;
+            rx_statistics_vector : OUT STD_LOGIC_VECTOR(27 DOWNTO 0);
+            rx_statistics_valid : OUT STD_LOGIC;
+            rx_mac_aclk : OUT STD_LOGIC;
+            rx_reset : OUT STD_LOGIC;
+            rx_axis_mac_tdata : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+            rx_axis_mac_tvalid : OUT STD_LOGIC;
+            rx_axis_mac_tlast : OUT STD_LOGIC;
+            rx_axis_mac_tuser : OUT STD_LOGIC;
+            tx_ifg_delay : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+            tx_statistics_vector : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+            tx_statistics_valid : OUT STD_LOGIC;
+            tx_mac_aclk : OUT STD_LOGIC;
+            tx_reset : OUT STD_LOGIC;
+            tx_axis_mac_tdata : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+            tx_axis_mac_tvalid : IN STD_LOGIC;
+            tx_axis_mac_tlast : IN STD_LOGIC;
+            tx_axis_mac_tuser : IN STD_LOGIC_VECTOR(0 DOWNTO 0);
+            tx_axis_mac_tready : OUT STD_LOGIC;
+            pause_req : IN STD_LOGIC;
+            pause_val : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+            speedis100 : OUT STD_LOGIC;
+            speedis10100 : OUT STD_LOGIC;
+            gmii_txd : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+            gmii_tx_en : OUT STD_LOGIC;
+            gmii_tx_er : OUT STD_LOGIC;
+            gmii_rxd : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+            gmii_rx_dv : IN STD_LOGIC;
+            gmii_rx_er : IN STD_LOGIC;
+            rx_configuration_vector : IN STD_LOGIC_VECTOR(79 DOWNTO 0);
+            tx_configuration_vector : IN STD_LOGIC_VECTOR(79 DOWNTO 0)
+        );
+    END COMPONENT;
+
+	signal gmii_txd, gmii_rxd: std_logic_vector(7 downto 0);
+	signal gmii_tx_en, gmii_tx_er, gmii_rx_dv, gmii_rx_er: std_logic;
+	signal gmii_rx_clk: std_logic;
+	signal sig_det, gt_clkp_i, gt_clkn_i: std_logic;
+	signal clk125, clk_fr: std_logic;
+	signal rstn, phy_done, s_mmcm_locked, locked_int: std_logic;
+	signal dc: std_logic := '0';
+	signal clk_dc: std_logic;
+	
+begin
+
+	clkp_buf: IBUF -- required because top level IBUF insertion does not work for IBUFDS_GT buried in PCS/PMA core
+		port map(
+			i => gt_clkp,
+			o => gt_clkp_i
+		);
+		
+	clkn_buf: IBUF
+		port map(
+			i => gt_clkn,
+			o => gt_clkn_i
+		);
+
+	clk125_fr <= clk_fr;
+	clk125_out <= clk125;
+
+	process(clk_fr)
+	begin
+		if rising_edge(clk_fr) then
+			locked_int <= s_mmcm_locked and phy_done;
+		end if;
+	end process;
+
+	locked <= locked_int;
+	rstn <= not (not locked_int or rsti);
+
+	mac: temac_gbe_v9_0
+		port map(
+			gtx_clk => clk125,
+			glbl_rstn => rstn,
+			rx_axi_rstn => '1',
+			tx_axi_rstn => '1',
+			rx_statistics_vector => open,
+			rx_statistics_valid => open,
+			rx_mac_aclk => open,
+			rx_reset => open,
+			rx_axis_mac_tdata => rx_data,
+			rx_axis_mac_tvalid => rx_valid,
+			rx_axis_mac_tlast => rx_last,
+			rx_axis_mac_tuser => rx_error,
+			tx_ifg_delay => X"00",
+			tx_statistics_vector => open,
+			tx_statistics_valid => open,
+			tx_mac_aclk => open,
+			tx_reset => open,
+			tx_axis_mac_tdata => tx_data,
+			tx_axis_mac_tvalid => tx_valid,
+			tx_axis_mac_tlast => tx_last,
+			tx_axis_mac_tuser(0) => tx_error,
+			tx_axis_mac_tready => tx_ready,
+			pause_req => '0',
+			pause_val => X"0000",
+			gmii_txd => gmii_txd,
+			gmii_tx_en => gmii_tx_en,
+			gmii_tx_er => gmii_tx_er,
+			gmii_rxd => gmii_rxd,
+			gmii_rx_dv => gmii_rx_dv,
+			gmii_rx_er => gmii_rx_er,
+			rx_configuration_vector => X"0000_0000_0000_0000_0812",
+			tx_configuration_vector => X"0000_0000_0000_0000_0012"
+		);
+
+-- This is pretty crap, but appears the only way to avoid vivado issues
+	
+	process(clk_fr)
+	begin
+		if rising_edge(clk_fr) then
+			dc <= not dc;
+		end if;
+	end process;
+
+	dc_buf: BUFG
+		port map(
+			i => dc,
+			o => clk_dc
+		);
+		
+    clk_indep <= clk_dc;
+    
+	phy: entity work.gig_eth_pcs_pma_basex_gtp
+		port map(
+			gtrefclk_p => gt_clkp_i,
+			gtrefclk_n => gt_clkn_i,
+			gtrefclk_out => gtrefclk_out,
+			gtrefclk_bufg_out => clk_fr,	
+			txp => gt_txp,
+			txn => gt_txn,
+			rxp => gt_rxp,
+			rxn => gt_rxn,
+			resetdone => phy_done,
+			
+			userclk_out => user_clk,
+			
+			userclk2_out => clk125,
+			rxuserclk_out => open,
+			rxuserclk2_out => open,
+			
+			pma_reset_out => pma_reset,
+			
+			mmcm_locked_out => s_mmcm_locked,
+			independent_clock_bufg => clk_dc,
+			gmii_txd => gmii_txd,
+			gmii_tx_en => gmii_tx_en,
+			gmii_tx_er => gmii_tx_er,
+			gmii_rxd => gmii_rxd,
+			gmii_rx_dv => gmii_rx_dv,
+			gmii_rx_er => gmii_rx_er,
+			gmii_isolate => open,
+			configuration_vector => "00000",
+			status_vector => open,
+			reset => rsti,
+			signal_detect => sig_det,
+			
+			gt0_pll0outclk_out => gt0_pll0outclk_in,
+			gt0_pll0outrefclk_out => gt0_pll0outrefclk_in,
+			
+			gt0_pll1outclk_out => gt0_pll1outclk_in,
+			gt0_pll1outrefclk_out => gt0_pll1outrefclk_in,
+			gt0_pll0refclklost_out => gt0_pll0refclklost_in,
+			gt0_pll0lock_out => gt0_pll0lock_in
+		);
+		
+    mmcm_locked <= s_mmcm_locked;
+
+	sig_det <= not sfp_los;
+
+end rtl;
---
+++ components/ipbus_util/firmware/hdl/clocks/clocks_7s_serdes_multi_gtps.vhd
@@ -0,0 +1,198 @@
+---------------------------------------------------------------------------------
+--
+--   Copyright 2017 - Rutherford Appleton Laboratory and University of Bristol
+--
+--   Licensed under the Apache License, Version 2.0 (the "License");
+--   you may not use this file except in compliance with the License.
+--   You may obtain a copy of the License at
+--
+--       http://www.apache.org/licenses/LICENSE-2.0
+--
+--   Unless required by applicable law or agreed to in writing, software
+--   distributed under the License is distributed on an "AS IS" BASIS,
+--   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--   See the License for the specific language governing permissions and
+--   limitations under the License.
+--
+--                                     - - -
+--
+--   Additional information about ipbus-firmare and the list of ipbus-firmware
+--   contacts are available at
+--
+--       https://ipbus.web.cern.ch/ipbus
+--
+---------------------------------------------------------------------------------
+
+
+-- clocks_7s_serdes
+--
+-- Input is a free-running 125MHz clock (taken straight from MGT clock buffer)
+--
+-- Dave Newbold, April 2011
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library unisim;
+use unisim.VComponents.all;
+
+entity clocks_7s_serdes is
+	generic(
+		CLK_FR_FREQ: real := 125.0;
+		CLK_VCO_FREQ: real := 1000.0; -- VCO freq 1000MHz
+		CLK_AUX_FREQ: real := 40.0
+	);
+	port(
+		clki_fr: in std_logic; -- Input free-running clock (125MHz)
+		clki_125: in std_logic; -- Ethernet domain clk125
+		clko_ipb: out std_logic; -- ipbus domain clock (31MHz)
+		clko_aux: out std_logic; -- auxiliary clock
+		clko_200: out std_logic; -- 200MHz clock for idelayctrl
+		eth_locked: in std_logic; -- ethernet locked signal
+		eth_bp_locked: in std_logic;
+		locked: out std_logic; -- global locked signal
+		nuke: in std_logic; -- hard reset input
+		soft_rst: in std_logic; -- soft reset input
+		rsto_125: out std_logic; -- clk125 domain reset (held until ethernet locked)
+		rsto_125_bp : out std_logic;
+		rsto_ipb: out std_logic; -- ipbus domain reset
+		rsto_aux: out std_logic; -- clk_aux domain reset (held until ethernet locked)
+		rsto_eth: out std_logic; -- ethernet startup reset (required!)
+		rsto_ipb_ctrl: out std_logic; -- ipbus domain reset for controller
+		rsto_fr: out std_logic; -- free-running clock domain reset
+		onehz: out std_logic -- blinkenlights output
+	);
+
+end clocks_7s_serdes;
+
+architecture rtl of clocks_7s_serdes is
+	
+	signal dcm_locked, sysclk, clk_ipb_i, clk_ipb_b, clkfb, clk200: std_logic;
+	signal clk_aux_i, clk_aux_b: std_logic;
+	signal d17, d17_d: std_logic;
+	signal nuke_i, nuke_d, nuke_d2, eth_done, eth_done_bp: std_logic := '0';
+	signal rst, srst, rst_ipb, rst_aux, rst_125, rst_125_bp, rst_ipb_ctrl: std_logic := '1';
+	signal rctr: unsigned(3 downto 0) := "0000";
+	attribute mark_debug : string;
+	attribute mark_debug of eth_bp_locked, eth_done_bp, eth_done, eth_locked, rst : signal is "true";
+begin
+	
+	sysclk <= clki_fr;
+
+	bufgipb: BUFG port map(
+		i => clk_ipb_i,
+		o => clk_ipb_b
+	);
+	
+	clko_ipb <= clk_ipb_b;
+	
+	bufgaux: BUFG port map(
+		i => clk_aux_i,
+		o => clk_aux_b
+	);
+	
+	clko_aux <= clk_aux_b;
+	
+	bufg200: BUFG port map(
+		i => clk200,
+		o => clko_200
+	);
+	
+	mmcm: MMCME2_BASE
+		generic map(
+			clkin1_period => 1000.0 / CLK_FR_FREQ,
+			clkfbout_mult_f => CLK_VCO_FREQ / CLK_FR_FREQ,
+			clkout1_divide => integer(CLK_VCO_FREQ / 31.25),
+			clkout2_divide => integer(CLK_VCO_FREQ / CLK_AUX_FREQ),
+			clkout3_divide => integer(CLK_VCO_FREQ / 200.00)
+		)
+		port map(
+			clkin1 => sysclk,
+			clkfbin => clkfb,
+			clkfbout => clkfb,
+			clkout1 => clk_ipb_i,
+			clkout2 => clk_aux_i,
+			clkout3 => clk200, -- No BUFG needed here, goes to idelayctrl on local routing
+			locked => dcm_locked,
+			rst => '0',
+			pwrdwn => '0'
+		);
+	
+	clkdiv: entity work.ipbus_clock_div
+		port map(
+			clk => sysclk,
+			d17 => d17,
+			d28 => onehz
+		);
+	
+	process(sysclk)
+	begin
+		if rising_edge(sysclk) then
+			d17_d <= d17;
+			if d17='1' and d17_d='0' then
+				rst <= nuke_d2 or not dcm_locked;
+				nuke_d <= nuke_i; -- ~1ms time bomb (allows return packet to be sent)
+				nuke_d2 <= nuke_d;
+				eth_done <= (eth_done or eth_locked) and not rst;
+				eth_done_bp <= (eth_done_bp or eth_bp_locked) and not rst;
+				rsto_eth <= rst; -- delayed reset for ethernet block to avoid startup issues
+			end if;
+		end if;
+	end process;
+	
+	locked <= dcm_locked;
+	srst <= '1' when rctr /= "0000" else '0';
+	
+	process(clk_ipb_b)
+	begin
+		if rising_edge(clk_ipb_b) then
+			rst_ipb <= rst or srst;
+			nuke_i <= nuke;
+			if srst = '1' or soft_rst = '1' then
+				rctr <= rctr + 1;
+			end if;
+		end if;
+	end process;
+	
+	rsto_ipb <= rst_ipb;
+	
+	process(clk_ipb_b)
+	begin
+		if rising_edge(clk_ipb_b) then
+			rst_ipb_ctrl <= rst;
+		end if;
+	end process;
+	
+	rsto_ipb_ctrl <= rst_ipb_ctrl;
+	
+	process(clk_aux_b)
+	begin
+		if rising_edge(clk_aux_b) then
+			rst_aux <= rst;
+		end if;
+	end process;
+	
+	rsto_aux <= rst_aux;
+
+	process(clki_125)
+	begin
+		if rising_edge(clki_125) then
+			rst_125 <= rst or not eth_done;
+		end if;
+	end process;
+	
+	rsto_125 <= rst_125;
+	
+    process(clki_125)
+	begin
+		if rising_edge(clki_125) then
+			rst_125_bp <= rst or not eth_done_bp;
+		end if;
+	end process;
+	
+	rsto_125_bp <= rst_125_bp;
+	
+	rsto_fr <= rst;
+		
+end rtl;
