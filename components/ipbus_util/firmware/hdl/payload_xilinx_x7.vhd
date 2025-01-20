071b41ff8ddeb5048ab896f7e29906a3d3fc3ebd
jhegeman <jeroen.hegeman@cern.ch>
2025-01-20 08:43:11 +0100

    Remove stale author/date stamp

--- components/ipbus_util/firmware/hdl/payload_xilinx_x7.vhd
+++ components/ipbus_util/firmware/hdl/payload_xilinx_x7.vhd
@@ -23,9 +23,6 @@
 --
 ---------------------------------------------------------------------------------
 
-
--- Jeroen Hegeman, December 2019
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use work.ipbus.all;
